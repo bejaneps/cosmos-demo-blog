6ec9e2052fd5cb4a0ccaa0e1e31c6f21d07d06af
bejaneps <bejanhtc@gmail.com>
2022-09-29 18:38:19 +0300

    Dirty commit

--- ts-client/blog.blog/registry.ts
+++ ts-client/blog.blog/registry.ts
@@ -1,6 +1,10 @@
 import { GeneratedType } from "@cosmjs/proto-signing";
+import { MsgCreateComment } from "./types/blog/tx";
+import { MsgCreatePost } from "./types/blog/tx";
 
 const msgTypes: Array<[string, GeneratedType]>  = [
+    ["/blog.blog.MsgCreateComment", MsgCreateComment],
+    ["/blog.blog.MsgCreatePost", MsgCreatePost],
     
 ];
 
